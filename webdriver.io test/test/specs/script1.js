5fd386691749b76a174dbec21c1c8d7f1bf1a521
agomezomg <agomez98@unitec.edu>
2021-01-03 12:24:32 -0600

    testing browser functions@

--- webdriver.io test/test/specs/script1.js
+++ webdriver.io test/test/specs/script1.js
@@ -5,10 +5,13 @@ describe("Test 1", () => {
 	it("Testing valid values.", () => {
 		browser.url("/parkcalc");
 		const drop_down_menu = $('#ParkingLot');
-		drop_down_menu.click();
-		console.log(drop_down_menu.getValue()); // returns "someValue0"
-		drop_down_menu.selectByIndex(4);
-		console.log(drop_down_menu.getValue());
-		
+		drop_down_menu.selectByIndex(0);
+		const start_date = $('#StartingDate');
+		const start_time = $('#StartingTime');
+		const leaving_date = $('#LeavingDate');
+		const leaving_time = $('#LeavingTime');
+		const radial_start_time = $('#StartingTimeAMPM');
+		const radial_leaving_time = $('#LeavingTimeAMPM');
+		browser.positionClick(browser.moveTo('#Submit', 71.58, 21));
 	})
 });
