205e4337fbbd57c16577943ebca5c3fa3c8970be
agomezomg <agomez98@unitec.edu>
2021-01-06 20:09:46 -0600

    minor changes'

---
+++ webdriver.io test/test/specs/BasicFunctionality.js
@@ -0,0 +1,147 @@
+const { addLabel } = require('@wdio/allure-reporter').default;
+const { addIssue } = require('@wdio/allure-reporter').default;
+/* 
+	Simple test to perform with valid values on items without changing radial buttons.
+*/
+
+beforeEach(() => {
+	/* Initilialising browser link and reloading before each test is performed.*/
+	browser.url("/parkcalc");
+});
+
+/*
+
+We want to test basic funcionalities with this. If all correct values are entered, then the application should
+print the total for parking according to the selected values.
+
+With this testing script, we are attempting to ascertain that basic validations and functionality are working properly.
+Basic functionality is the printing of the total without problem. Basic validations are that all fields (especially
+text fields) are entered correctly.
+
+*/
+
+describe("Testing pricing according to parking type (basic functionality)", () => {
+	it("Testing functionality with default values, changing date only.", () => {
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('0/0/0');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('0/0/0');
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+
+
+		/* Should result in something like $12 (0 Days, 0 Hours, 59 minutes) */
+		const result = $('span*=$ 12.00');
+		expect(result).toExist();
+		addIssue('Allows dates that have long since passed...like 0/0/0');
+		addLabel('Basic Test #1');
+	});
+
+	it("Testing for Short-Term Parking.", () => {
+		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
+		const drop_down_menu = $('#ParkingLot');
+		drop_down_menu.selectByIndex(1);
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('03/01/2020');
+		/* Creates a constant for Starting Time field and sets value to a valid time. */		
+		const start_time = $('#StartingTime');
+		start_time.setValue('12:00');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('03/01/2020');
+		/* Creates a constant for Leaving Time field and sets value to a valid time. */
+		const leaving_time = $('#LeavingTime');
+		leaving_time.setValue('12:59');
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+
+		/* Should result in something like $ 2.00 (0 Days, 0 Hours, 59 minutes) */
+		const result = $('span*=$ 2.00');
+		expect(result).toExist();
+		addLabel('Basic Test #2');
+	});
+
+	it("Testing for Economy Parking.", () => {
+		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
+		const drop_down_menu = $('#ParkingLot');
+		drop_down_menu.selectByIndex(2);
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('03/01/2020');
+		/* Creates a constant for Starting Time field and sets value to a valid time. */		
+		const start_time = $('#StartingTime');
+		start_time.setValue('12:00');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('03/01/2020');
+		/* Creates a constant for Leaving Time field and sets value to a valid time. */
+		const leaving_time = $('#LeavingTime');
+		leaving_time.setValue('12:59');
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+
+		/* Should result in something like $ 2.00 (0 Days, 0 Hours, 59 minutes) */
+		const result = $('span*=$ 2.00');
+		expect(result).toExist();
+		addLabel('Basic Test #3');
+	});
+
+	it("Testing for Short-Term Parking.", () => {
+		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
+		const drop_down_menu = $('#ParkingLot');
+		drop_down_menu.selectByIndex(3);
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('03/01/2020');
+		/* Creates a constant for Starting Time field and sets value to a valid time. */		
+		const start_time = $('#StartingTime');
+		start_time.setValue('10:00');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('03/01/2020');
+		/* Creates a constant for Leaving Time field and sets value to a valid time. */
+		const leaving_time = $('#LeavingTime');
+		leaving_time.setValue('12:59');
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+
+		/* Should result in something like $ 2.00 (0 Days, 2 Hours, 59 minutes) */
+		const result = $('span*=$ 2.00');
+		expect(result).toExist();
+		addLabel('Basic Test #3');
+		addIssue('Check the Days, Hours, and Minutes stamp');
+	});
+
+	it("Testing for Long-Term Parking.", () => {
+		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
+		const drop_down_menu = $('#ParkingLot');
+		drop_down_menu.selectByIndex(4);
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('03/01/2020');
+		/* Creates a constant for Starting Time field and sets value to a valid time. */		
+		const start_time = $('#StartingTime');
+		start_time.setValue('09:00');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('03/01/2020');
+		/* Creates a constant for Leaving Time field and sets value to a valid time. */
+		const leaving_time = $('#LeavingTime');
+		leaving_time.setValue('11:30');
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+
+		/* Should result in something like $ 2.00 (0 Days, 2 Hours, 59 minutes) */
+		const result = $('span*=$ 6.00');
+		expect(result).toExist();
+		addLabel('Basic Test #4');
+	});
+});
---
+++ webdriver.io test/test/specs/DateChecking.js
@@ -0,0 +1,64 @@
+const { addLabel } = require('@wdio/allure-reporter').default;
+const { addIssue } = require('@wdio/allure-reporter').default;
+
+beforeEach(() => {
+	/* Initilialising browser link and reloading before each test is performed.*/
+	browser.url("/parkcalc");
+});
+
+/*
+
+It is important to check that the inputed user values make sense timewise, too. This test can help us make sure that
+slotted times are not inputed incorrectly.
+
+*/
+describe("Checking valid date and time stamps", () => {
+	/* This test checks only that both timestamps, set in AM, are valid. */
+	it("Checking validation for leaving date not happening before starting date", () => {
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('03/01/2020');
+		/* Creates a constant for Starting Time field and sets value to a valid time. */		
+		const start_time = $('#StartingTime');
+		start_time.setValue('12:59');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('02/01/2020');
+		/* Creates a constant for Leaving Time field and sets value to a valid time. */
+		const leaving_time = $('#LeavingTime');
+		leaving_time.setValue('12:50');
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+		
+		const result
+	});
+
+	/* This test checks that the timestamps are valid, with the starting time being set in PM
+	and the leaving time set in AM within the same date.*/
+	it("Checking validation for leaving date not happening before starting date", () => {
+		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
+		const drop_down_menu = $('#ParkingLot');
+		drop_down_menu.selectByIndex(3);
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('03/01/2020');
+		/* Creates a constant for Starting Time field and sets value to a valid time. */		
+		const start_time = $('#StartingTime');
+		start_time.setValue('12:59');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('03/01/2020');
+		/* Creates a constant for Leaving Time field and sets value to a valid time. */
+		const leaving_time = $('#LeavingTime');
+		leaving_time.setValue('12:50');
+		/* Creates an elem array for Radial Buttons and toggles values according to position. */
+		const radial_STime = $$('[name="StartingTimeAMPM"]');
+		radial_STime[1].click();
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+
+		
+	});
+});
