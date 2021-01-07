dfc00757e5f6ffbcd5f03557e236c5b9f9eb87ca
agomezomg <agomez98@unitec.edu>
2021-01-06 18:37:26 -0600

    found an error and found a fix!

--- webdriver.io test/test/specs/BasicValidations.js
+++ webdriver.io test/test/specs/BasicValidations.js
@@ -1,9 +1,11 @@
-import allureReporter from '@wdio/allure-reporter';
-
-
+//import allureReporter from '@wdio/allure-reporter';
+const { addLabel } = require('@wdio/allure-reporter').default;
+const { addSeverity } = require('@wdio/allure-reporter').default;
+const { addIssue } = require('@wdio/allure-reporter').default;
 /* 
 	Simple test to perform with valid values on items.
 */
+
 beforeEach(() => {
 	/* Initilialising browser link and reloading before each test is performed.*/
 	browser.url("/parkcalc");
@@ -21,7 +23,52 @@ text fields) are entered correctly.
 */
 
 describe("Testing Basics", () => {
-	it("Testing functionality with valid values.", () => {
+	it("Testing functionality with default values, changing date only.", () => {
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('0/0/0');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('0/0/0');
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+
+
+		/* Should result in something like $12 (0 Days, 0 Hours, 59 minutes) */
+		const result = $('span*=$ 12.00');
+		expect(result).toExist();
+		addIssue('Allows dates that have long since passed...like 0/0/0');
+		addLabel('Basic Test #1');
+	});
+
+	it("Testing functionality with valid values with combobox set to Short-Term Parking.", () => {
+		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
+		const drop_down_menu = $('#ParkingLot');
+		drop_down_menu.selectByIndex(1);
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
+		const start_date = $('#StartingDate');
+		start_date.setValue('03/01/2020');
+		/* Creates a constant for Starting Time field and sets value to a valid time. */		
+		const start_time = $('#StartingTime');
+		start_time.setValue('12:00');
+		/* Creates a constant for Leaving Date field and sets value to a valid date. */
+		const leaving_date = $('#LeavingDate');
+		leaving_date.setValue('03/01/2020');
+		/* Creates a constant for Leaving Time field and sets value to a valid time. */
+		const leaving_time = $('#LeavingTime');
+		leaving_time.setValue('12:59');
+		/* Creates a constant for Submit Button and performs a send. */
+		const submitButton = $('[name="Submit"]');
+		submitButton.click();
+
+		/* Should result in something like $ 2.00 (0 Days, 0 Hours, 59 minutes) */
+		const result = $('span*=$ 2.00');
+		expect(result).toExist();
+		addLabel('Basic Test #2');
+	});
+
+	it("Testing functionality with valid values with combobox set to Economy Parking.", () => {
 		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
 		const drop_down_menu = $('#ParkingLot');
 		drop_down_menu.selectByIndex(2);
@@ -30,7 +77,7 @@ describe("Testing Basics", () => {
 		start_date.setValue('03/01/2020');
 		/* Creates a constant for Starting Time field and sets value to a valid time. */		
 		const start_time = $('#StartingTime');
-		start_time.setValue('10:51');
+		start_time.setValue('12:00');
 		/* Creates a constant for Leaving Date field and sets value to a valid date. */
 		const leaving_date = $('#LeavingDate');
 		leaving_date.setValue('03/01/2020');
@@ -41,23 +88,22 @@ describe("Testing Basics", () => {
 		const submitButton = $('[name="Submit"]');
 		submitButton.click();
 
-
-		/* Should result in something like $12 (0 Days, 0 Hours, 59 minutes) */
-		const result = $('*=59 Minutes');
-		console.log(result.getText());
-		allureReporter.addSeverity('Critical');
+		/* Should result in something like $ 2.00 (0 Days, 0 Hours, 59 minutes) */
+		const result = $('span*=$ 2.00');
+		expect(result).toExist();
+		addLabel('Basic Test #3');
 	});
 
-	it("Testing format compliance with starting date text field", () => {
+	it("Testing functionality with valid values with combobox set to Economy Parking.", () => {
 		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
 		const drop_down_menu = $('#ParkingLot');
-		drop_down_menu.selectByIndex(0);
-		/* Creates a constant for Starting Date field and sets value to an invalid date. */
+		drop_down_menu.selectByIndex(2);
+		/* Creates a constant for Starting Date field and sets value to a valid date. */
 		const start_date = $('#StartingDate');
-		start_date.setValue('03/012020');
+		start_date.setValue('03/01/2020');
 		/* Creates a constant for Starting Time field and sets value to a valid time. */		
 		const start_time = $('#StartingTime');
-		start_time.setValue('10:51');
+		start_time.setValue('12:00');
 		/* Creates a constant for Leaving Date field and sets value to a valid date. */
 		const leaving_date = $('#LeavingDate');
 		leaving_date.setValue('03/01/2020');
@@ -68,96 +114,9 @@ describe("Testing Basics", () => {
 		const submitButton = $('[name="Submit"]');
 		submitButton.click();
 
-		/* Should result in ERROR! ENTER A CORRECTLY FORMATTED DATE */
-		const result = $('=ERROR! ENTER A CORRECTLY FORMATTED DATE');
-		console.log(result.getText());
-		allureReporter.addSeverity('Critical');
-	});
-
-	it("Testing format compliance with starting time text field", () => {
-		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
-		const drop_down_menu = $('#ParkingLot');
-		drop_down_menu.selectByIndex(0);
-		/* Creates a constant for Starting Date field and sets value to a valid date. */
-		const start_date = $('#StartingDate');
-		start_date.setValue('03/012020');
-		/* Creates a constant for Starting Time field and sets value to an invalid time. */		
-		const start_time = $('#StartingTime');
-		start_time.setValue('10?');
-		/* Creates a constant for Leaving Date field and sets value to a valid date. */
-		const leaving_date = $('#LeavingDate');
-		leaving_date.setValue('03/01/2020');
-		/* Creates a constant for Leaving Time field and sets value to a valid time. */
-		const leaving_time = $('#LeavingTime');
-		leaving_time.setValue('15:51');
-		/* Creates an elem array for Radial Buttons and toggles values according to position. */
-		const radial_STime = $$('[name="StartingTimeAMPM"]');
-		radial_STime[1].click();
-		/* Creates a constant for Submit Button and performs a send. */
-		const submitButton = $('[name="Submit"]');
-		submitButton.click();
-
-		/* Should result in ERROR! ENTER A CORRECTLY FORMATTED DATE */
-		const result = $('=ERROR! ENTER A CORRECTLY FORMATTED DATE');
-		console.log(result.getText());
-		allureReporter.addSeverity('Critical');
-	});
-
-	it("Testing format compliance with leaving date text field", () => {
-		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
-		const drop_down_menu = $('#ParkingLot');
-		drop_down_menu.selectByIndex(0);
-		/* Creates a constant for Starting Date field and sets value to a valid date. */
-		const start_date = $('#StartingDate');
-		start_date.setValue('03/012020');
-		/* Creates a constant for Starting Time field and sets value to a valid time. */		
-		const start_time = $('#StartingTime');
-		start_time.setValue('10:00');
-		/* Creates a constant for Leaving Date field and sets value to an invalid date. */
-		const leaving_date = $('#LeavingDate');
-		leaving_date.setValue('03/2020');
-		/* Creates a constant for Leaving Time field and sets value to a valid time. */
-		const leaving_time = $('#LeavingTime');
-		leaving_time.setValue('15:51');
-		/* Creates an elem array for Radial Buttons and toggles values according to position. */
-		const radial_STime = $$('[name="StartingTimeAMPM"]');
-		radial_STime[1].click();
-		/* Creates a constant for Submit Button and performs a send. */
-		const submitButton = $('[name="Submit"]');
-		submitButton.click();
-
-		/* Should result in ERROR */
-		const result = $('*=ERROR');
-		console.log(result.getText());
-		allureReporter.addSeverity('Critical');
-	});
-
-	it("Testing format compliance with leaving time text field", () => {
-		/* Creates a constant for dropdown menu and retrieves available, clickable values. */
-		const drop_down_menu = $('#ParkingLot');
-		drop_down_menu.selectByIndex(0);
-		/* Creates a constant for Starting Date field and sets value to a valid date. */
-		const start_date = $('#StartingDate');
-		start_date.setValue('03/012020');
-		/* Creates a constant for Starting Time field and sets value to a valid time. */		
-		const start_time = $('#StartingTime');
-		start_time.setValue('10:00');
-		/* Creates a constant for Leaving Date field and sets value to a valid date. */
-		const leaving_date = $('#LeavingDate');
-		leaving_date.setValue('03/01/2020');
-		/* Creates a constant for Leaving Time field and sets value to an invalid time. */
-		const leaving_time = $('#LeavingTime');
-		leaving_time.setValue('1551');
-		/* Creates an elem array for Radial Buttons and toggles values according to position. */
-		const radial_STime = $$('[name="StartingTimeAMPM"]');
-		radial_STime[1].click();
-		/* Creates a constant for Submit Button and performs a send. */
-		const submitButton = $('[name="Submit"]');
-		submitButton.click();
-
-		/* Should result in ERROR */
-		const result = $('*=ERROR');
-		console.log(result.getText());
-		allureReporter.addSeverity('Critical');
+		/* Should result in something like $ 2.00 (0 Days, 0 Hours, 59 minutes) */
+		const result = $('span*=$ 2.00');
+		expect(result).toExist();
+		addLabel('Basic Test #2');
 	});
 });
